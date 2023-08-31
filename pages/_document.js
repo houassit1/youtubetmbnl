ece45fb414fefe030269569cef3b4583633998cf
houassit1 <ouassithamza1@gmail.com>
2023-08-31 17:33:39 +0100

    seotext

--- pages/_document.js
+++ pages/_document.js
@@ -34,30 +34,32 @@ class MyDocument extends Document {
             YouTube (HD, HQ, 1080p, 4K) videos
           </p>
 
-          <p>
-            Welcome to YouTube Thumbnail Downloader! Are you looking to
-            retrieve high-quality thumbnails from your favorite YouTube videos?
-            You're in the right place. Our user-friendly platform allows you to
-            easily download thumbnails in different resolutions, including Full
-            HD (1080p), HD (720p), and more. Here's how it works:
-          </p>
-
-          <ul>
-            <li>Copy the URL of the YouTube video you want to extract a thumbnail from.</li>
-            <li>Paste the URL into the designated area on our website.</li>
-            <li>Click the "Download" button.</li>
-            <li>Choose the desired thumbnail resolution and format.</li>
-            <li>Enjoy your downloaded thumbnail!</li>
-          </ul>
-
-          <p>
-            Whether you're a content creator, a marketer, or simply a YouTube enthusiast, our YouTube Thumbnail Downloader provides you with quick and easy access to the thumbnails you need. Feel free to explore and start downloading your favorite video thumbnails today!
-          </p>
-
-          {/* ... continue adding the rest of the provided content ... */}
-
           <Main />
           <NextScript />
+
+          <div style={{
+            margin: "2rem auto",
+            textAlign: "center",
+            border: "2px dashed #999",
+            padding: "1rem",
+            maxWidth: "600px",
+          }}>
+            <p style={{ fontSize: "1.2rem", marginBottom: "1rem" }}>
+              Welcome to YouTube Thumbnail Downloader!
+              Are you looking to retrieve high-quality thumbnails from your favorite YouTube videos?
+              You're in the right place. Our user-friendly platform allows you to easily download thumbnails in different resolutions, including Full HD (1080p), HD (720p), and more. Here's how it works:
+            </p>
+            <ol style={{ textAlign: "left" }}>
+              <li>Copy the URL of the YouTube video you want to extract a thumbnail from.</li>
+              <li>Paste the URL into the designated area on our website.</li>
+              <li>Click the "Download" button.</li>
+              <li>Choose the desired thumbnail resolution and format.</li>
+              <li>Enjoy your downloaded thumbnail!</li>
+            </ol>
+            <p>
+              Whether you're a content creator, a marketer, or simply a YouTube enthusiast, our YouTube Thumbnail Downloader provides you with quick and easy access to the thumbnails you need. Feel free to explore and start downloading your favorite video thumbnails today!
+            </p>
+          </div>
         </body>
       </Html>
     );
